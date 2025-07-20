91162df2cb2d5719374d7307bc0979b92062dbba
nadaabdelazim633 <nadaabdelazim89@gmail.com>
2025-07-21 01:55:25 +0300

    Create login_screen.dart

---
+++ login-screen/login_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final phoneController = TextEditingController();
+    final passwordController = TextEditingController();
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const SizedBox(height: 80),
+            const Text(
+              'أهلا بك نحن سعداء بعودتك\nمن فضلك قم بتسجيل الدخول',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            TextField(
+              controller: phoneController,
+              decoration: const InputDecoration(
+                hintText: 'رقم الهاتف',
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: 'كلمة المرور',
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text('هل نسيت كلمة المرور؟'),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/otp');
+              },
+              child: const Text('تسجيل الدخول'),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text('ليس لديك حساب؟'),
+                SizedBox(width: 5),
+                Text('إنشاء حساب', style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
