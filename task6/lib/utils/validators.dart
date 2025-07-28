1ef8efa78a8595d7eb2d5e6addd35a6b8238fcac
nadaabdelazim633 <nadaabdelazim89@gmail.com>
2025-07-28 16:44:03 +0300

    Create validators.dart

---
+++ task6/lib/utils/validators.dart
@@ -0,0 +1,19 @@
+class Validators {
+  static String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) return 'Please enter your email';
+    if (!value.contains('@')) return 'Please enter a valid email';
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) return 'Please enter your password';
+    if (value.length < 6) return 'Password must be at least 6 characters';
+    return null;
+  }
+
+  static String? validateConfirmPassword(String? value, String password) {
+    if (value == null || value.isEmpty) return 'Please confirm your password';
+    if (value != password) return 'Passwords do not match';
+    return null;
+  }
+}
